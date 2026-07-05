cc3511ba27371186ec735ffc409f185ba360fe16
RouSage <rousage@protonmail.com>
2026-07-05 16:38:25 +0200

    REPL loop [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,12 +7,19 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		// Wait for user input
+		fmt.Print("$ ")
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+		// Read the command
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+
+		// Print the error message
+		fmt.Printf("%s: command not found\n", command[:len(command)-1])
 	}
-	fmt.Printf("%s: command not found\n", command[:len(command)-1])
 }
