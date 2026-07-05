7364445d787925b6bfe2311b8461352072009aa4
RouSage <rousage@protonmail.com>
2026-07-05 16:34:40 +0200

    Handle invalid commands [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Print("$ ")
+
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s: command not found\n", command[:len(command)-1])
 }
