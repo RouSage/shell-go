cbcd1207d9e513eb927b86fb88241722cd0f84e4
RouSage <rousage@protonmail.com>
2026-07-26 14:45:03 +0200

    Multiple completions [skip ci]

--- app/shell/completer.go
+++ app/shell/completer.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 
 	"github.com/chzyer/readline"
 )
 
 type Completer struct {
 	readline.AutoCompleter
+	belledFor string
+	belled    bool
 }
 
 func NewCompleter(completers ...readline.PrefixCompleterInterface) *Completer {
@@ -42,9 +45,37 @@ func NewCompleter(completers ...readline.PrefixCompleterInterface) *Completer {
 
 func (c *Completer) Do(line []rune, pos int) ([][]rune, int) {
 	newLine, length := c.AutoCompleter.Do(line, pos)
+
 	if len(newLine) == 0 {
-		fmt.Fprintf(os.Stderr, "%c", 0x07)
+		c.reset()
+		bell()
+		return nil, 0
+	}
+	if len(newLine) == 1 {
+		c.reset()
+		return newLine, length
 	}
 
+	sort.Slice(newLine, func(i, j int) bool {
+		return string(newLine[i]) < string(newLine[j])
+	})
+
+	if !(c.belled && c.belledFor == string(line)) {
+		c.belled = true
+		c.belledFor = string(line)
+		bell()
+		return nil, 0
+	}
+
+	c.reset()
 	return newLine, length
+}
+
+func (c *Completer) reset() {
+	c.belled = false
+	c.belledFor = ""
+}
+
+func bell() {
+	fmt.Fprintf(os.Stderr, "%c", 0x07)
 }
